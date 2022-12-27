46038e65ffb36898842a4cdeffc63d8f132f6190
seungok Jeong <seungok.jeong@elicer.com>
2022-12-27 10:34:02 +0900

    feat: add rushstack/eslint-patch

--- base.js
+++ base.js
@@ -1,3 +1,5 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
     extends: ['airbnb-base'],
     parserOptions: { ecamVersion: 'latest' },
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 const rules = {
   'react/function-component-definition': [
     'error',
